458b408e4a5cca213f3b935871448fd5558f7949
Dmitry <d@kozlov.ru>
2025-02-24 00:01:42 +0700

    Added auth/verify api

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ import { Service } from 'hub-service'
 const keychain = new Keychain()
 await keychain.load()
 
-new Service().start()
+new Service().post('auth/verify', body => keychain.verify(body)).start()
